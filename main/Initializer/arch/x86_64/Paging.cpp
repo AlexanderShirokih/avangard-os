3c4c0532c618116b01b5e3e8e280a5e33f802347
Alexander Shirokih <quandastudio@mail.ru>
2018-09-17 16:30:25 +0300

    add page mapping functions

--- main/Initializer/arch/x86_64/Paging.cpp
+++ main/Initializer/arch/x86_64/Paging.cpp
@@ -7,27 +7,146 @@
 
 #include <arch/x86_64/paging.h>
 
-#include <std/format.h>
+#include <sys/system.h>
 
-void System::PageEntry::print(Std::OutputStream *out)
+#include <std/format.h>
+#include <std/log.h>
+
+using namespace System;
+
+/**
+ * Kernel PML4 Table address that always be the same.
+ */
+PageEntry *tranlsationTable = (PageEntry *)0xFFFFFFFFFFFFF000;
+
+/**
+ * Prints info about the page entry to OutputStream
+ */
+void PageEntry::print(Std::OutputStream *out)
 {
-    Std::printf(out, "PageEntry[prsnt=%u, wtr=%u, usr=%u, wrtThr=%u, nCache=%u, acc=%u, drty=%u, huge|Null=%u, glb=%u, noEx=%u, physAddr=%X]\n", isPresent, isWritable, isUserAccessible, isWriteThroughCaching, isNoCache, isAccessed, isDirty, isHugeOrNull, isGlobal, isNoExecute, physicalAddress);
+    Std::printf(out, "PageEntry[prsnt=%u, wtr=%u, usr=%u, wrtThr=%u, nCache=%u, acc=%u, drty=%u, huge|Null=%u, glb=%u, noEx=%u, physAddr=%X]\n", isPresent, isWritable, isUserAccessible, isWriteThroughCaching, isNoCache, isAccessed, isDirty, isHugeOrNull, isGlobal, isNoExecute, getPhysicalAddress());
 }
 
 /**
- * Returns next level pointer of PageEntry or null this entry is a huge page or not present
+ * Returns next level pointer of PageEntry.
+ * This method is not checking for the page isPresent.
  */
-System::PageEntry *System::PageEntry::getNextLevelEntry(UInt index)
+PageEntry *PageEntry::getNextLevelEntry(UInt index)
 {
-    if (isPresent && !isHugeOrNull)
+    Address addr = reinterpret_cast<Address>(this);
+    return (PageEntry *)((addr << 9) | (index << 12));
+}
+
+PageEntry *PageEntry::getOrCreate(UInt index, System::PhysMemory *allocator)
+{
+    //We can not create child tables in huge page
+    kassert(!isHugeOrNull);
+
+    debug("getOrCreate for:");
+    print(getStdOut());
+
+    PageEntry * next = getNextLevelEntry(index);  
+    if (!this[index].value)
     {
-        Address addr = *((Address *)this);
-        return (PageEntry *)((addr << 9) | (index << 12));
+        debugf("allocating frame for index=%i\n", index);
+        this[index].value = allocator->allocFrame();
+        debugf("allocated physicalAddress=%X\n", this[index].value);
+        //TODO Debug
+        next->fillZero();
     }
+
+    return next;
+}
+
+/**
+ * Returns 52-bit aligned physical address or null is this page is not present.
+ */
+Address PageEntry::getPhysicalAddress()
+{
+    if (isPresent)
+        return value & 0x000FFFFFFFFFF000;
     return null;
 }
 
-void System::VirtualAddress::print(Std::OutputStream *out)
+void PageEntry::fillZero()
 {
-    Std::printf(out, "VirtualAddress[PMLi=%u, PDPi=%u, PDi=%u, PTi=%u, Offset=%u, SignEx=%u]\n", p4Index, p3Index, p2Index, p1Index, offset, signExt);
+    for (UInt i = 0; i < ENTRY_COUNT; i++)
+    {
+        //physicalAddress is a largest variable of this union
+        this[i].value = 0x0;
+    }
+}
+
+/**
+ * Prints info about the virtual address to OutputStream
+ */
+void VirtualAddress::print(Std::OutputStream *out)
+{
+    Std::printf(out, "VirtualAddress[PMLi=%u, PDPi=%u, PDi=%u, {PTi=%u, Offset=%u}|{Offset2M=%u}, SignEx=%u]\n", p4Index, p3Index, p2Index, p1Index, offset, offset2M, signExt);
+}
+
+/**
+ * Returns physical address from virtual or 0 if virtual address is not present
+ */
+Address VirtualMemory::getPhysicalAddress(VirtualAddress *address)
+{
+    // translationTable should be always present
+    PageEntry *l3 = tranlsationTable->getNextLevelEntry(address->p4Index);
+
+    //Note that 1GiB page is not supported
+    if (l3->isPresent)
+    {
+
+        PageEntry *l2 = l3->getNextLevelEntry(address->p3Index);
+
+        if (l2->isPresent) //if l2 page is present
+        {
+            PageEntry *l1OrPage = &l2[address->p2Index];
+
+            if (l1OrPage->isPresent)
+            {
+                if (l1OrPage->isHugeOrNull) //It's a 2MiB huge page
+                {
+                    return l1OrPage->getPhysicalAddress() + address->offset2M;
+                }
+
+                PageEntry *page = l1OrPage->getNextLevelEntry(address->p1Index);
+                if (page->isPresent)
+                    return page->getPhysicalAddress() + address->offset;
+            }
+        }
+    }
+
+    return null;
+}
+
+void VirtualMemory::mapDirect(Address virtAddr, Address physAddress, UInt pageSize, Bool isWritable, Bool isUser, Bool isExceutable)
+{
+    kassert(pageSize == HUGE_PAGE_SIZE || pageSize == NORMAL_PAGE_SIZE);
+    kassert((physAddress % pageSize == 0) && (virtAddr % pageSize == 0));
+
+    VirtualAddress address(virtAddr);
+    PhysMemory *allocator = PhysMemory::instance();
+
+    PageEntry *l3 = tranlsationTable->getOrCreate(address.p4Index, allocator);
+    PageEntry *l2 = l3->getOrCreate(address.p3Index, allocator);
+
+    PageEntry *l1OrPage = &l2[address.p2Index];
+    PageEntry *page;
+
+    if (pageSize == HUGE_PAGE_SIZE)
+    {
+        page = l1OrPage;
+        page->isHugeOrNull = true;
+    }
+    else
+    {
+        page = l1OrPage->getOrCreate(address.p1Index, allocator);
+    }
+
+    kassert(page->value == 0);
+    page->isPresent = true;
+    page->isWritable = isWritable;
+    page->isUserAccessible = isUser;
+    page->isNoExecute = !isExceutable;
 }
--- main/Initializer/include/arch/x86_64/paging.h
+++ main/Initializer/include/arch/x86_64/paging.h
@@ -8,30 +8,39 @@
 #ifndef INCLUDE_PAGING
 #define INCLUDE_PAGING
 
+#include <sys/phys_mem.h>
+
 #include <std/types.h>
 #include <std/streams.h>
+
+#define ENTRY_COUNT 512
 
 namespace System
 {
 
-struct __attribute__((packed)) PageEntry
-{
-    Bool isPresent : 1;             //0
-    Bool isWritable : 1;            //1
-    Bool isUserAccessible : 1;      //2
-    Bool isWriteThroughCaching : 1; //3
-    Bool isNoCache : 1;             //4
-    Bool isAccessed : 1;            //5
-    Bool isDirty : 1;               //6
-    Bool isHugeOrNull : 1;          //7
-    Bool isGlobal : 1;              //8
-    UByte unused1 : 3;              //9-11
-    Address physicalAddress : 40;   //12-52
-    UShort unused2 : 11;            //53-62
-    Bool isNoExecute : 1;           //63
+union PageEntry {
+    struct __attribute__((packed))
+    {
+        Bool isPresent : 1;             //0
+        Bool isWritable : 1;            //1
+        Bool isUserAccessible : 1;      //2
+        Bool isWriteThroughCaching : 1; //3
+        Bool isNoCache : 1;             //4
+        Bool isAccessed : 1;            //5
+        Bool isDirty : 1;               //6
+        Bool isHugeOrNull : 1;          //7
+        Bool isGlobal : 1;              //8
+        ULong skipped : 54;             //9-62
+        Bool isNoExecute : 1;           //63
+    };
 
-    void print(Std::OutputStream *os);
+    Address value;
+
     PageEntry *getNextLevelEntry(UInt index);
+    PageEntry *getOrCreate(UInt index, PhysMemory *allocator);
+    Address getPhysicalAddress();
+    void print(Std::OutputStream *os);
+    void fillZero();
 };
 
 union VirtualAddress {
@@ -45,6 +54,11 @@ union VirtualAddress {
         UInt signExt : 16;
     };
 
+    struct __attribute__((packed))
+    {
+        UInt offset2M : 21;
+    };
+
     Address address;
 
     void print(Std::OutputStream *os);
@@ -53,8 +67,14 @@ union VirtualAddress {
     VirtualAddress(Address addr) { address = addr; }
 };
 
+#define HUGE_PAGE_SIZE (1 << 21)
+#define NORMAL_PAGE_SIZE (1 << 12)
+
 struct VirtualMemory
 {
+    Address getPhysicalAddress(VirtualAddress *addr);
+
+    void mapDirect(Address virtAddr, Address physAddress, UInt pageSize, Bool isWritable, Bool isUser, Bool isExceutable);
 };
 
 } // namespace System
